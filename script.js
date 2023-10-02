23e7ae19a0cd21b86e69319cc6915de5a88d0e82
nacho arevalo <elnachoamsn@gmail.com>
2023-10-01 23:03:02 -0300

    Code-challenge

---
+++ script.js
@@ -0,0 +1,27 @@
+bton.addEventListener('click', function(e){
+    e.preventDefault()
+    const name = document.getElementById("name").value;
+    const lastname = document.getElementById("lastname").value;
+    const date = document.getElementById("date").value;
+
+    const dataJSON = {
+        nombre: name,
+        apellido: lastname,
+        fechadenacimiento: date,
+    };
+
+    fetch('https://jsonplaceholder.typicode.com/users', {
+        method:"post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataJSON),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
+});
